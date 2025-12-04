b93c4c05a76807f4e658a918ab4813898636ee78
amruthashiv <154325406+amruthashiv@users.noreply.github.com>
2025-12-04 11:52:56 +0530

    Updated the App title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Fin AI",
-  description: "Finance Transaction Management",
+  title: "Fin-AI",
+  description: "Finance management powered by AI",
 };
 
 export default function RootLayout({
